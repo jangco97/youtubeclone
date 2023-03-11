5717b1240cd5112e645c3628b1cebadba7f45b82
geon woo <qkrnslehd@naver.com>
2023-03-11 17:42:36 +0900

    channelpage.js 만들기

---
+++ src/pages/ChannelPage/index.js
@@ -0,0 +1,17 @@
+import axios from '../../api/axios';
+import requests from '../../api/requests';
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+const ChannelPage = () => {
+  //   let { channelId } = useParams();
+  //   const [channelData, setChannelData] = useState([]);
+  //   const getChannelVideo = async () => {
+  //     await axios.get(requests.fetchChannelData(channelId)).then(res => {
+  //       res;
+  //     });
+  //   };
+  return <div></div>;
+};
+
+export default ChannelPage;
