436af90cef65a3e1b8d3505e8b905100c88eacbb
Valentin1495 <newface95@icloud.com>
2023-03-11 19:23:38 +0900

    Hamburger menu 수정

--- src/components/HeaderComponents/Nav.js
+++ src/components/HeaderComponents/Nav.js
@@ -12,14 +12,18 @@ const Nav = ({ setShowSidebar }) => {
   return (
     <div className='nav'>
       <div className='svg-img'>
-        <svg
-          className='icon'
-          xmlns='http://www.w3.org/2000/svg'
-          viewBox='0 0 448 512'
+        <button
+          className='hamburger-btn'
           onClick={() => setShowSidebar((prev) => !prev)}
         >
-          <path d='M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z' />
-        </svg>
+          <svg
+            className='hamburger'
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 448 512'
+          >
+            <path d='M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z' />
+          </svg>
+        </button>
 
         <img
           src='https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg'
