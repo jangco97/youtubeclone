05e8a115864f2708b792b96f35bb1eb81d8360f9
geon woo <qkrnslehd@naver.com>
2023-03-16 19:11:46 +0900

    api 가리기

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: 'https://youtube.googleapis.com/youtube/v3',
   params: {
-    key: 'AIzaSyABoFjUWUHFVLrR8Fb56qn1rR3t-HrTggQ',
+    key: '사용자 api',
   },
 });
 
